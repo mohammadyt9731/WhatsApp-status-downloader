4b12862ea5e12169373a1d992bf9f247ae81dab9
Mohammad Yazdi <mohammadyt1379@gmail.com>
2022-03-31 19:58:03 +0430

    add sdp and ssp library add dimens.xml design activity_splash.xml handle SplashActivity.kt

--- app/src/main/java/com/test/whatsappstatusdowloader/SplashActivity.kt
+++ app/src/main/java/com/test/whatsappstatusdowloader/SplashActivity.kt
@@ -1,11 +1,28 @@
 package com.test.whatsappstatusdowloader
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+import kotlin.concurrent.timerTask
 
 class SplashActivity : AppCompatActivity() {
+
+    private var DURATION:Long = 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        goToMainActivity()
+    }
+
+    private fun goToMainActivity() {
+
+        Timer().schedule(timerTask {
+
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }, DURATION)
     }
 }
