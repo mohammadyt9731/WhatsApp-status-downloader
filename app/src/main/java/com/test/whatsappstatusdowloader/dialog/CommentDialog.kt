845ac9cff3565dd3fdfa437a4a21557eefbf3c42
Mohammad Yazdi <mohammadyt1379@gmail.com>
2022-05-05 16:06:40 +0430

    update view and logic of CommentDialog

--- app/src/main/java/com/test/whatsappstatusdowloader/dialog/CommentDialog.kt
+++ app/src/main/java/com/test/whatsappstatusdowloader/dialog/CommentDialog.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.view.View
 import android.widget.AbsListView
 import android.widget.RatingBar
+import android.widget.Toast
 import com.test.whatsappstatusdowloader.R
 import com.test.whatsappstatusdowloader.databinding.DialogCommentBinding
 import com.test.whatsappstatusdowloader.utils.Constants
@@ -38,29 +39,35 @@ class CommentDialog(context: Context) : Dialog(context) {
             ratingBar.onRatingBarChangeListener =
                 RatingBar.OnRatingBarChangeListener { _, v, _ ->
                     if (v == 0f) {
-                        btnComment.setBackgroundColor(context.getColor(R.color.dark_green))
                         tvCommentCaffeBazzar.visibility = View.GONE
-                        btnComment.isEnabled = false
+                        btnComment.text=context.getString(R.string.comment)
+                        btnComment.alpha=0.5f
                     } else {
-                        btnComment.setBackgroundColor(context.getColor(R.color.green))
                         tvCommentCaffeBazzar.visibility = View.VISIBLE
-                        btnComment.isEnabled = true
+                        btnComment.alpha=1f
+
+                        if (v < 4) {
+                            btnComment.text = context.getString(R.string.send_email)
+                            tvCommentCaffeBazzar.text = context.getString(R.string.please_send_email)
+                        } else {
+                            tvCommentCaffeBazzar.text = context.getString(R.string.please_register_your_comment)
+                            btnComment.text = context.getString(R.string.comment)
+                        }
                     }
-                    if (v < 4) {
-                        btnComment.text = context.getString(R.string.send_email)
-                        tvCommentCaffeBazzar.text = context.getString(R.string.please_send_email)
-                    } else {
-                        tvCommentCaffeBazzar.text = context.getString(R.string.please_register_your_comment)
-                        btnComment.text = context.getString(R.string.comment)
-                    }
+
                 }
-            btnClose.setOnClickListener(View.OnClickListener { cancel() })
+            ivClose.setOnClickListener(View.OnClickListener { cancel() })
             btnComment.setOnClickListener(View.OnClickListener {
-                if (ratingBar.getRating() < 4) {
-                    MyIntent.emailIntent(context)
-                } else {
-                    MyIntent.commentIntent(context)
+
+                when(ratingBar.rating){
+                    0f-> {
+                        Toast.makeText(context,context.getString(R.string.submitt_comment),Toast.LENGTH_SHORT).show()
+                        return@OnClickListener
+                    }
+                    in 1f..3f->MyIntent.emailIntent(context)
+                    else->MyIntent.commentIntent(context)
                 }
+
                 cancel()
             })
         }
